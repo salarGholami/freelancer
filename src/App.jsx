1228920c8459cfd84dbc543610596a7f22500e77
salarGholami <salargholami021@gmail.com>
2024-09-27 01:02:11 +0330

    Send OTP handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="container xl:max-w-screen-xl">
-      <Routes>
-        <Route path="/auth" element={<Auth />}></Route>
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <div className="container xl:max-w-screen-xl">
+        <Routes>
+          <Route path="/auth" element={<Auth />}></Route>
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import TextFiled from "../../ui/TextFiled";
+import { useMutation } from "@tanstack/react-query";
+import { getOtp } from "../../services/authService";
+import toast from "react-hot-toast";
 
 function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
 
-  const sendOtpHandler = (e) => {
+  const { isPending, error, data, mutateAsync } = useMutation({
+    mutationFn: getOtp,
+  });
+
+  const sendOtpHandler = async (e) => {
     e.preventDefault();
+    try {
+      const data = await mutateAsync({ phoneNumber });
+      console.log(data.message);
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
   };
+
   return (
     <div>
       <form className="space-y-4" onSubmit={sendOtpHandler}>
@@ -16,7 +31,9 @@ function SendOTPForm() {
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
-        <button type="submit" className="btn btn--primary w-full">ارسال کد تایید</button>
+        <button type="submit" className="btn btn--primary w-full">
+          ارسال کد تایید
+        </button>
       </form>
     </div>
   );
