8dba9f1ce1c8577e2e3a1556790baea11441adfd
salarGholami <salargholami021@gmail.com>
2024-10-11 05:45:38 +0330

    Owner Sidebar

---
+++ src/pages/OwnerDashboard.jsx
@@ -0,0 +1,7 @@
+function OwnerDashboard() {
+  return (
+    <div>OwnerDashboard page</div>
+  )
+}
+
+export default OwnerDashboard
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,9 +1,42 @@
+import { HiCollection, HiHome } from "react-icons/hi";
+import { NavLink } from "react-router-dom";
+
 function Sidebar() {
   return (
-    <div className="bg-secondary-0 row-start-1 row-span-2">
-      <h1>sideBar</h1>
+    <div className="bg-secondary-0 row-start-1 row-span-2 border-l border-gray-200 p-4">
+      <ul className="flex flex-col gap-y-4">
+        <li>
+          <CustomNaveLink to="/owner/dashboard">
+            <HiHome />
+            <span>داشبورد</span>
+          </CustomNaveLink>
+        </li>
+        <li>
+          <CustomNaveLink to="/owner/projects">
+            <HiCollection />
+            <span>پروژه ها</span>
+          </CustomNaveLink>
+        </li>
+      </ul>
     </div>
   );
 }
 
 export default Sidebar;
+
+function CustomNaveLink({ children, to }) {
+  const navLinkClass =
+    "flex items-center gap-x-2 hover:bg-primary-100/50 hover:text-primary-900 px-2 py-1.5 rounded-lg transition-all duration-300";
+  return (
+    <NavLink
+      className={({ isActive }) =>
+        isActive
+          ? `${navLinkClass} bg-primary-100/50 text-primary-900`
+          : `${navLinkClass} text-secondary-600`
+      }
+      to={to}
+    >
+      {children}
+    </NavLink>
+  );
+}
