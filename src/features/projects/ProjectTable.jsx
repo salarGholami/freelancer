91a6ff5f5adfed0d845d5f3c8902cf9b84c0680e
salarGholami <salargholami021@gmail.com>
2024-10-15 02:40:10 +0330

    Compound Component Table

--- src/features/projects/ProjectTable.jsx
+++ src/features/projects/ProjectTable.jsx
@@ -1,8 +1,8 @@
 import Empty from "../../ui/Empty";
 import Loading from "../../ui/Loading";
-import toLocalDateShort from "../../utils/toLocalDateShort";
-import { toPersianNumberWithComma } from "../../utils/toPersianNumber";
-import truncateText from "../../utils/truncateText";
+import Table from "../../ui/Table";
+import ProjectRow from "./ProjectRow";
+
 import useOwnerProjects from "./useOwnerProjects";
 
 function ProjectTable() {
@@ -13,52 +13,24 @@ function ProjectTable() {
   if (!projects.length) return <Empty resourceName="پروژه" />;
 
   return (
-    <div className="bg-secondary-0 overflow-x-auto">
-      <table>
-        <thead>
-          <tr className="title-row">
-            <th>#</th>
-            <th>عنوان پروژه</th>
-            <th>دسته بندی</th>
-            <th>بودجه</th>
-            <th>ددلاین</th>
-            <th>تگ ها</th>
-            <th>فریلنسر</th>
-            <th>وضعیت</th>
-            <th>عملیات</th>
-          </tr>
-        </thead>
-        <tbody>
-          {projects.map((project, index) => (
-            <tr key={project._id}>
-              <td>{index + 1}</td>
-              <td>{truncateText(project.title, 30)}</td>
-              <td>{project.category.title}</td>
-              <td>{toPersianNumberWithComma(project.budget)}</td>
-              <td>{toLocalDateShort(project.deadline)}</td>
-              <td>
-                <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
-                  {project.tags.map((tag) => (
-                    <span className="badge badge--secondary" key={tag}>
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-              </td>
-              <td>{project.freelancer?.name || "-"}</td>
-              <td>
-                {project.status === "OPEN" ? (
-                  <span className="badge badge--success">باز</span>
-                ) : (
-                  <span className="badge badge--danger">بسته</span>
-                )}
-              </td>
-              <td>...</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Table>
+      <Table.Header>
+        <th>#</th>
+        <th>عنوان پروژه</th>
+        <th>دسته بندی</th>
+        <th>بودجه</th>
+        <th>ددلاین</th>
+        <th>تگ ها</th>
+        <th>فریلنسر</th>
+        <th>وضعیت</th>
+        <th>عملیات</th>
+      </Table.Header>
+      <Table.Body>
+        {projects.map((project, index) => (
+          <ProjectRow index={index} project={project} key={project._id} />
+        ))}
+      </Table.Body>
+    </Table>
   );
 }
 
