3fa12dce8b671c1748f5169c4978f3cd24afae45
salarGholami <salargholami021@gmail.com>
2024-10-25 19:06:02 +0330

    proposalTable

--- src/features/project/ProposalsTable.jsx
+++ src/features/project/ProposalsTable.jsx
@@ -1,8 +1,27 @@
+import Empty from "../../ui/Empty";
+import Table from "../../ui/Table";
+import ProposalRow from "./ProposalRow";
 
-function ProposalsTable() {
+function ProposalsTable({ proposals }) {
+  if (!proposals.length) return <Empty resourceName="درخواستی" />;
   return (
-    <div>ProposalsTable</div>
-  )
+    <Table>
+      <Table.Header>
+        <th>#</th>
+        <th>فریلنسر</th>
+        <th>توضیحات</th>
+        <th>زمان تحویل</th>
+        <th>هزینه</th>
+        <th>وضعیت</th>
+        <th>عملیات</th>
+      </Table.Header>
+      <Table.Body>
+        {proposals.map((proposal, index) => (
+          <ProposalRow index={index} proposal={proposal} key={proposal._id} />
+        ))}
+      </Table.Body>
+    </Table>
+  );
 }
 
-export default ProposalsTable
+export default ProposalsTable;
