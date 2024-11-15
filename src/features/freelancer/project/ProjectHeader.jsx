17f433e164fdffe45840530e3fbeed48f2281f3a
salarGholami <salargholami021@gmail.com>
2024-11-15 22:32:26 +0330

    Filter Component

--- src/features/freelancer/project/ProjectHeader.jsx
+++ src/features/freelancer/project/ProjectHeader.jsx
@@ -1,5 +1,26 @@
 import useCategories from "../../../hooks/useCategories";
+import Filter from "../../../ui/Filter";
 import FilterDropDown from "../../../ui/FilterDropDown";
+
+const sortOptions = [
+  { value: "latest", label: "مرتب سازی ( جدید ترین )" },
+  { value: "earliest", label: "مرتب سازی ( قدیمی ترین )" },
+];
+
+const statusOptions = [
+  {
+    value: "ALL",
+    label: "همه",
+  },
+  {
+    value: "OPEN",
+    label: "باز",
+  },
+  {
+    value: "CLOSED",
+    label: "بسته",
+  },
+];
 
 function ProjectHeader() {
   const { transformedCategories } = useCategories();
@@ -7,6 +28,8 @@ function ProjectHeader() {
     <div className="flex items-center justify-between text-secondary-700 mb-8">
       <h1 className="text-lg font-bold">لیست پروژه ها</h1>
       <div className="flex gap-x-8 items-center">
+        <Filter filterFiled="status" options={statusOptions} />
+        <FilterDropDown filterFiled="sort" options={sortOptions} />
         <FilterDropDown
           filterFiled="category"
           options={[
